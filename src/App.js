a9f8b8d13e8cc0285e8ad8fdee9b2a0c6dd29cde
Ramya <ramyakpass@gmail.com>
2025-04-22 18:45:00 -0400

    React intro

--- src/App.js
+++ src/App.js
@@ -2,21 +2,54 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //creating objects in js
+  const product1={
+    name:"Apple",
+    price:99999999000
+  }
+  const product2={
+    name:"Samsung",
+    price:25000
+  }
+  console.log(product1);
+  console.log(product2);
+
+// creating array
+const productlist=[product1,product2];
+console.log(productlist);
+console.log(productlist[0].name);
+
+//creating function
+
+//function declaration
+function add(num1,num2){
+  return num1+num2;
+}
+console.log(add(5,2));
+
+//function expression
+const mul=function(x,y){
+return x*y;
+}
+console.log(mul(5,2));
+          
+//arrow function
+const addnumbers=(num1,num2) => num1+num2;
+console.log(addnumbers(5,2));
+
+//array.map()- used to iterate numbers one by one
+const numbers=[1,2,3,4,5];
+numbers.map((num)=>console.log(num*2));
+
+productlist.map((prod)=>{
+console.log(prod.name);
+console.log(prod.price);
+})
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
